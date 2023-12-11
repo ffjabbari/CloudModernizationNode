7fe4ac7f69416837238f4f00f0e859b188cb28cf
Fred Jabbari <fjabbari@emoneyadvisor.com>
2023-12-11 13:06:31 -0500

    aadded operations

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("This is my express app");
+  res.send("Queries");
 });
 
-app.get("/me", (req, res) => {
-  res.send("Hi I am Laith");
+app.get("/op001", (req, res) => {
+  res.send("Query 001");
+});
+
+app.get("/op002", (req, res) => {
+  res.send("Query 002");
 });
 
 app.listen(5000, () => {
-  console.log("listening");
+  console.log("listening on internal Docker port 5000");
 });
